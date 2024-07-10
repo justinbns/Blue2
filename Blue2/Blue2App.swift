cf6e9bedcce503df245c524c0848445db3deabce
justinbns <stayheylow@gmail.com>
2024-07-10 11:27:19 +0700

    Initial Commit

---
+++ Blue2/Blue2App.swift
@@ -0,0 +1,17 @@
+//
+//  Blue2App.swift
+//  Blue2
+//
+//  Created by Justin Jefferson on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Blue2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blue2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blue2
+//
+//  Created by Justin Jefferson on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
