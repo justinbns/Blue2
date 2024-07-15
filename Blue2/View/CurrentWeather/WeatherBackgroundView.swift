60ad9a4b4832b8df4cfc9f80bdc6697492896885
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-07-15 11:26:27 +0700

    Create Background View

---
+++ Blue2/View/CurrentWeather/WeatherBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherBackgroundView.swift
+//  Blue2
+//
+//  Created by Anthony on 15/07/24.
+//
+
+import SwiftUI
+
+struct WeatherBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherBackgroundView()
+}
