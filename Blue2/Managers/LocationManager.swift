8f50e0a6ab7a7c2f0a6148cca7b558846c38e127
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-07-15 10:09:09 +0700

    Feature: Adding Corelocation Capability

---
+++ Blue2/Managers/LocationManager.swift
@@ -0,0 +1,62 @@
+//
+//  LocationManager.swift
+//  Blue2
+//
+//  Created by Anthony on 15/07/24.
+//
+
+import Foundation
+import CoreLocation
+
+protocol LocationManagerDelegate: AnyObject {
+    func didUpdateLocation(latitude: Double, longitude: Double, cityName: String)
+    func didFailWithError(error: Error)
+}
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let locationManager = CLLocationManager()
+    weak var delegate: LocationManagerDelegate?
+    
+    var authorizationStatus: CLAuthorizationStatus {
+        return locationManager.authorizationStatus
+    }
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.requestLocation()
+        case .restricted, .denied, .notDetermined:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            if let error = error {
+                self.delegate?.didFailWithError(error: error)
+            } else if let placemark = placemarks?.first, let city = placemark.locality {
+                self.delegate?.didUpdateLocation(
+                    latitude: location.coordinate.latitude,
+                    longitude: location.coordinate.longitude,
+                    cityName: city
+                )
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        delegate?.didFailWithError(error: error)
+    }
+}
